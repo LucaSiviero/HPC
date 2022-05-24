a0b24ff1d9ac0767545b1f49b38247234da11f2b
LucaSiviero <lucasivi.998@gmail.com>
2022-05-24 10:47:19 +0200

    Refactoring

--- project/run_container.sh
+++ project/run_container.sh
@@ -1,3 +1,3 @@
 #! /bin/sh
 
-docker run -it -v /home/luca/hpc/src:/root/src --gpus=all luigicrisci/sycl_cuda11.4
+docker run -it -v /home/luca/HPC/project:/root/src --gpus=all luigicrisci/sycl_cuda11.4
